a5bc250ab3f4f4b1c29222137cc6fa080922469b
Liren Tu <ltu@liveramp.com>
2018-10-25 18:54:08 +0800

    Test uniqueness of value type values

--- src/test/java/com/github/tuliren/json_tuple/TestValueType.java
+++ src/test/java/com/github/tuliren/json_tuple/TestValueType.java
@@ -1,16 +1,26 @@
 package com.github.tuliren.json_tuple;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class TestValueType {
+public class TestValueType extends BaseTestCase {
 
   @Test
   public void testValidValue() {
     for (ValueType valueType : ValueType.values()) {
       assertEquals(valueType, ValueType.findByValue(valueType.value));
     }
+  }
+
+  @Test
+  public void testUniqueValue() {
+    Set<Integer> values = Arrays.stream(ValueType.values()).map(v -> v.value).collect(Collectors.toSet());
+    assertEquals(ValueType.values().length, values.size());
   }
 
   @Test(expected = IllegalArgumentException.class)
